489612dc43973e73d31578c7d01908dd5100fd86
Gruuben33 <ruubenpuddle@gmail.com>
2026-05-20 13:34:52 -0400

    edited button class

--- buttons.js
+++ buttons.js
@@ -1,5 +1,13 @@
-class Button(x, y) {
-    constructor(){
-
+class Button {
+    constructor(position, purpose, text, color){
+        this.position = position;
+        this.purpose = purpose;
+        this.text = text;
+        this.color = color;
     } 
+
+    draw() {
+        fill(this.color)
+        square(this.position.x, this.position.y, 40)
+    }
 }
